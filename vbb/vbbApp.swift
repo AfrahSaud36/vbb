5724ccde52f0543c55b903cde24e16f238c707fc
AfrahSaud36 <afrahsaud36@gmail.com>
2024-09-26 11:52:39 +0300

    Initial Commit

---
+++ vbb/vbbApp.swift
@@ -0,0 +1,17 @@
+//
+//  vbbApp.swift
+//  vbb
+//
+//  Created by Afrah Alharbi on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct vbbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
